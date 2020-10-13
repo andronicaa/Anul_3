7a5c965e1f04faef84b60d2246463a0ce8516e0a
andronicaa <alexandraandronic368@gmail.com>
2020-10-13 18:44:23 +0300

    tutorial .net

---
+++ Semestrul_1/DAW/Laborator 2/Laborator 2/Controllers/FiguriController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laborator_2.Models;
+
+namespace Laborator_2.Controllers
+{
+    public class FiguriController : Controller
+    {
+        // GET: Figuri
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Prima()
+        {
+            Figura f = new Figura();
+            f.Nume = "Cerc";
+            return View(f);
+        }
+    }
+}
