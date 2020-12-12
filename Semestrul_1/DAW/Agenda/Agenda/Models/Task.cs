efd19342e44e4d30d9e429face90a41173964ab9
andronicaa <alexandraandronic368@gmail.com>
2020-12-12 12:24:06 +0200

    Task model

---
+++ Semestrul_1/DAW/Agenda/Agenda/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string  Sarcina{ get; set; }
+        public string Prioritate { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
