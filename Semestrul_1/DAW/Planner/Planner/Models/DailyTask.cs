59d15900f86c3be0454dd45f913ce909ea015ac6
Alexandra Andronic <alexandraandronic368@gmail.com>
2020-12-20 14:54:09 +0200

    DailyTask Model, Migration and CRUD

---
+++ Semestrul_1/DAW/Planner/Planner/Models/DailyTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Planner.Models
+{
+    public class DailyTask
+    {
+        public int DailyTaskId { get; set; }
+        [Required(ErrorMessage = "Camp obligatoriu")]
+        [StringLength(15, ErrorMessage = "Titlul trebuie sa fie de lungime cuprinsa intre 4 si 15.", MinimumLength = 4)]
+        public string TitluTask { get; set; }
+        [Required(ErrorMessage = "Camp obligatoriu")]
+        public string Prioritate { get; set; }
+        [Required(ErrorMessage = "Camp obligatoriu")]
+        public string Deadline { get; set; }
+
+        [Required]
+        public string Detalii { get; set; }
+    }
+}
