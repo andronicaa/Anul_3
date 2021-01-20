db037048406faf625ba02d9bd779abc54765e06b
andronicaa <alexandraandronic368@gmail.com>
2021-01-20 11:38:38 +0200

    tema

--- Semestrul_1/CN/Tema/Exercitiul_2_integrala.py
+++ Semestrul_1/CN/Tema/Exercitiul_2_integrala.py
@@ -8,55 +8,68 @@ def f(x, sigma):
 
 
 def integrare(f, x, metoda):
-    sigma = 1.8
+    sigma = 1.9
     n = len(x)
     y = np.zeros((n, 1))
-
+    # print(np.shape(y))
     # calculam y
     for i in range(len(x)):
         y[i] = f(x[i], sigma)
 
-    # calculam h-ul(echidistant)
-    h = x[1] - x[0]
 
     if metoda == 'dreptunghi':
-        I = 2 * h * np.sum(y[::2])
+        m = (int)((n - 1) / 2)
+        h = (x[2 * m] - x[0]) / (2 * m)
+        sum = 0
+        for k in range(m):
+            sum = sum + y[2 * k + 1]
+        I = 2 * h * sum
+
     elif metoda == 'trapez':
-        I = (h / 2) * (y[0] + 2 * np.sum(y[1:-2]) + y[-1])
+        m = n - 1
+        h = (x[m] - x[0]) / m
+        sum = 0
+        for k in range(1, m):
+            sum = sum + y[k]
+        I = (h / 2) * (y[0] + 2 * sum + y[m])
     elif metoda == 'simpson':
-        I = (h / 3) * (y[0] + 4 * np.sum(y[1:-1:2]) + 2 * np.sum(y[2:-1:2]) + y[-1])
+        m = (int)((n - 1) / 2)
+        h = (x[2 * m] - x[0]) / (2 * m)
+        sum_par = 0
+        sum_impar = 0
+        for k in range(m):
+            sum_par = sum_par + y[2 * k + 1]
+        for k in range(1, m):
+            sum_impar = sum_impar + y[2 * k]
+        
+        I = (h / 3) * (y[0] + 4 * sum_par + 2 * sum_impar + y[2 * m])
+        
+        
 
     return I
 
 
 def aplica_metode(metoda):
-    mrg_inf = -18
-    mrg_sup = 18
-    a = 3
-    b = 20
+    mrg_inf = -19
+    mrg_sup = 19
+    a = 5
+    b = 15
     rez_integrala = []
-    if metoda == 'dreptunghi' or metoda == 'simpson':
-        for N in range(a, b, 2):
-            x = np.linspace(mrg_inf, mrg_sup, N)
-            I = integrare(f, x, metoda)
-            rez_integrala.append(I)
-    else:
-        for N in range(a, b, 1):
-            x = np.linspace(mrg_inf, mrg_sup, N)
-            I = integrare(f, x, metoda)
-            rez_integrala.append(I)
+    for N in range(a, b, 1):
+        x = np.linspace(mrg_inf, mrg_sup, N)
+        I = integrare(f, x, metoda)
+        rez_integrala.append(I)
     return rez_integrala
 
 
 def plot_resp_metode():
-    a = 3
-    b = 20 
-    interval_ds = range(a, b, 2)
-    interval_trp = range(a, b, 1)
+    a = 5
+    b = 15
+    interval_ab = range(a, b, 1)
     plt.figure("Aproximarea integralei folosind formule de cuadratura sumate")
-    plt.plot(interval_ds, aplica_metode('dreptunghi'), label="dreptunghi")
-    plt.plot(interval_trp, aplica_metode('trapez'), label="trapez")
-    plt.plot(interval_ds, aplica_metode('simpson'), label="simpson")
+    plt.plot(interval_ab, aplica_metode('dreptunghi'), label="dreptunghi")
+    plt.plot(interval_ab, aplica_metode('trapez'), label="trapez")
+    plt.plot(interval_ab, aplica_metode('simpson'), label="simpson")
     plt.legend()
     plt.show()
 
